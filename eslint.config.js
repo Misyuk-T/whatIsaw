b078748f2163f7781ab4a2723790c8423057f302
Taras <taras.misiuk@anyforsoft.com>
2025-05-13 12:26:39 +0300

    FE: setup code quality toolchain
        - Add .nvmrc to pin Node.js version
        - Configure Prettier via .prettierrc and .prettierignore
        - Introduce lint-staged.config.js to run Prettier and ESLint on staged TS/JS files
        - Add Husky pre-commit hook to invoke lint-staged
        - Update eslint.config.js with React Native, TypeScript, import sorting, and Prettier integration
        - Update package.json scripts: prepare, format, lint fixes

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,3 @@
-// https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
 
@@ -6,5 +5,60 @@ module.exports = defineConfig([
   expoConfig,
   {
     ignores: ['dist/*'],
+    plugins: {
+      react: require('eslint-plugin-react'),
+      'react-native': require('eslint-plugin-react-native'),
+      '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+      'simple-import-sort': require('eslint-plugin-simple-import-sort'),
+      prettier: require('eslint-plugin-prettier'),
+    },
+    languageOptions: {
+      parser: require('@typescript-eslint/parser'),
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        ecmaVersion: 2021,
+        sourceType: 'module',
+        project: ['./tsconfig.json'],
+        tsconfigRootDir: __dirname,
+      },
+    },
+    settings: {
+      react: { version: 'detect' },
+      'import/resolver': {
+        typescript: { project: './tsconfig.json' },
+      },
+    },
+
+    rules: {
+      'jsx-quotes': ['error', 'prefer-single'],
+      'react/react-in-jsx-scope': 'off',
+      'no-param-reassign': 'off',
+      'import/prefer-default-export': 'off',
+      'arrow-body-style': ['error', 'as-needed'],
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'react/function-component-definition': [
+        'warn',
+        { namedComponents: 'arrow-function', unnamedComponents: 'arrow-function' },
+      ],
+      'react/require-default-props': 'off',
+      'react/jsx-props-no-spreading': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/naming-convention': 'off',
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            ['^react', '^@?\\w'],
+            ['^@/'],
+            ['^components', '^hooks', '^utils', '^constants', '^assets'],
+            ['^\\.'],
+          ],
+        },
+      ],
+      'react-native/no-unused-styles': 'warn',
+      'react-native/split-platform-components': 'error',
+      'react-native/no-raw-text': 'off',
+      'prettier/prettier': 'error',
+    },
   },
 ]);
